afbff672fc25b858b21d2ddf1ef0d12099a597b1
shuklaadi <2222adityashukla@gmail.com>
2021-03-18 07:34:07 +0000

    initial commit

---
+++ questionhard.js
@@ -0,0 +1,72 @@
+let questions for hard level = [
+    {
+    numb: 1,
+    question: "How can we describe an array in the best possible way?",
+    answer: "Container that stores the elements of similar types",
+    options: [
+      "The Array shows a hierarchical structure"
+      "Arrays are immutable"
+      "Container that stores the elements of similar types"
+      "The Array is not a data structure"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is the correct way of declaring an array?",
+    answer: "int javatpoint[10];",
+    options: [
+       "int javatpoint[10];"
+       "javatpoint{20};"
+       "array javatpoint[10];"
+       "int javatpoint;"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How can we initialize an array in C language?",
+    answer: "int arr[2] = {10, 20}",
+    options: [
+       "int arr[2]=(10, 20)"
+       "int arr(2)={10, 20}"
+       "int arr[2] = {10, 20}"
+       "int arr(2) = (10, 20)" 
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following highly uses the concept of an array?",
+    answer: "Spatial locality",
+    options: [
+        "Binary Search tree"
+        "Caching"
+        "Spatial locality"
+        "Scheduling of Processes"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which one of the following is the process of inserting an element in the stack?",
+    answer: "Push",
+    options: [
+        "Insert"
+        "Add"
+        "Push"
+        "None of the above"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ questionmedium.js
@@ -0,0 +1,57 @@
+let questions for medium level = [
+    {
+    numb: 1,
+    question: "Whic of the following is not a Java feature?",
+    answer: "Use of pointers",
+    options: [
+      "Dynamic",
+      "Architecture Neural",
+      "Use of pointers",
+      "Object Oriented"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is used to fix bugs in the java programs?",
+    answer: "JDB",
+    options: [
+      "JVM"
+      "JRE",
+      "JDK",
+      "JDB"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the return type of hashCode() method in Object class?",
+    answer: "int",
+    options: [
+      "Object",
+      "int",
+      "long",
+      "void"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does the expression float a= 35/0 return?",
+    answer: "Infinity",
+    options: [
+      "0",
+      "Not a number",
+      "Infinity",
+      "Run time exception"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is a valid declaration of a char?",
+    answer: "char ch = '\utea';",
+    options: [
+      "char cr = \u0223;",
+      "char ca = 'tea';",
+      "char ch = '\utea';",
+      "char cc = '\itea'"
+    ]
+  }
+  ];
